02e2cf761d869678a1cbf8cdc8d7a78e1fa97d4e
harm04 <harshmali.dev@gmail.com>
2026-01-31 23:52:30 +0530

    create backend folder and moved all the files into it. create an API to return a list of jokes.

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+//get a list of jokes
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "A Joke",
+      content:
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    },
+    {
+      id: 2,
+      title: "Another Joke",
+      content:
+        "Why don't scientists trust atoms? Because they make up everything!",
+    },
+    {
+      id: 3,
+      title: "One More Joke",
+      content:
+        "Why did the chicken join a band? Because it had the drumsticks!",
+    },
+  ];
+  res.json(jokes);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
