61621a9c0291b06ad569610efd863d3ce9145c0d
harm04 <harshmali.dev@gmail.com>
2026-02-04 15:49:54 +0530

    connect mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-dotenv.config();
+dotenv.config({
+  path: "./backend/.env",
+});
 const app = express();
 const port = process.env.PORT || 4000;
 const host = process.env.HOST;
 
-//get a list of jokes
-app.get("/api/jokes", (req, res) => {
-  const jokes = [
-    {
-      id: 1,
-      title: "A Joke",
-      content:
-        "Why did the scarecrow win an award? Because he was outstanding in his field!",
-    },
-    {
-      id: 2,
-      title: "Another Joke",
-      content:
-        "Why don't scientists trust atoms? Because they make up everything!",
-    },
-    {
-      id: 3,
-      title: "One More Joke",
-      content:
-        "Why did the chicken join a band? Because it had the drumsticks!",
-    },
-  ];
-  res.json(jokes);
-});
+//approach 2: connect to database first in different file, then start the server
 
-app.listen(port, () => {
-  console.log(`App listening at ${host}:${port}`);
-});
+connectDB();
+
+// approach 1 : function to connect to database and start the server
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (err) => {
+//       console.error(
+//         "Database connection error. App cannot talk to database:",
+//         err
+//       );
+//     });
+//     app.listen(port, () => {
+//       console.log(`App listening at ${host}:${port}`);
+//     });
+//   } catch (error) {
+//     console.error("Error connecting to the database:", error);
+//   }
+// })();
