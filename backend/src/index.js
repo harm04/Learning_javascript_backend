5334174896492a44aaed6bb71f23a7a1541e1deb
harm04 <harshmali.dev@gmail.com>
2026-02-05 22:57:48 +0530

    setup dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
 //start the server
 
 //listen to server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
