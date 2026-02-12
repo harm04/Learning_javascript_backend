c0002c5224fdf67ebee7c28ebb168c8e787a72bf
harm04 <harshmali.dev@gmail.com>
2026-02-12 20:02:29 +0530

    authentication completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,22 +4,35 @@ const dotenv = require("dotenv").config();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const authJwt = require("./middlewares/jwt.middleware");
+const errorHandler = require("./middlewares/error_handler.middleware");
 
 //variables
 const app = express();
 const env = process.env;
 const port = env.PORT || 3000;
 
-const API= env.API_URL;
+const API = env.API_URL;
 
 //middleware
+app.use(
+  cors({
+    origin: "*", // Allow all origins for testing
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
+app.use(authJwt());
+app.use(errorHandler);
 
 //routes
 const authRouter = require("./routes/auth.route");
 
 app.use(`${API}/`, authRouter);
+
 
 //connect to mongodb
 mongoose
